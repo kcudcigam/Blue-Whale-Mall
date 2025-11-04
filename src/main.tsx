e7beac5ff00a133bae542357ecd0e7ec93a7d704
magicduck <184408039@qq.com>
2025-11-04 18:35:17 +0800

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../App';
-import '../styles/globals.css';
-import { Toaster } from '../components/ui/sonner';
+import App from './App';
+import './styles/globals.css';
+import { Toaster } from './components/ui/sonner';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
